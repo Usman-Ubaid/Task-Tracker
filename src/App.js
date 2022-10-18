04fe8937bc2c3a3c8156419baf5a370324e2ebb9
Usman Ubaid <usmanubaid98@outlook.com>
2022-10-18 17:00:32 +0200

    Added the Tasks Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor\'s Appointment',
+        day: '20th Oct at 11:30',
+        reminder: true
+    }, 
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Oct 25th at 9:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shoppig',
+        day: 'Oct 28th at 16:00',
+        reminder: false
+    }
+])
+
   return (
     <div className="container">
       <Header title = 'Task Tracker' />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
-    return <button style={{backgroundColor:'green'}} className="btn">{text}</button>
-};
+const Button = ({ color, text, onClick }) => {
+    return ( 
+    <button onClick={onClick} style={{backgroundColor:'green'}} 
+    className="btn">
+        {text}
+    </button>
+)};
 
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
 }
 
 export default Button;
