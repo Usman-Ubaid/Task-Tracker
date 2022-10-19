40a2cfea50adb659875615c718aae9350e68b1c7
Usman Ubaid <usmanubaid98@outlook.com>
2022-10-19 13:01:09 +0200

    Added Toggle Reminder Functionality

--- src/App.js
+++ src/App.js
@@ -30,10 +30,21 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {...task, reminder:!task.reminder} 
+          : task))
+  }
+
   return (
     <div className="container">
       <Header title = 'Task Tracker' />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 
+      ? <Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder}/> 
+      : 'No Tasks to Show'}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
 import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className="task">
-            <h3>{task.text}<FaTimes className='icon' onClick={() => onDelete(task.id)}/></h3>
+        <div className={`task ${task.reminder ? 'task-reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+            <h3>{task.text}<FaTimes className='icon' 
+            onClick={() => onDelete(task.id)}/></h3>
             <p>{task.day}</p>
         </div>
     )
