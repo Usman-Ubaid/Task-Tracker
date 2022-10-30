34f2b43339c29c7accd9e17232a1da4434f4bfc5
Usman Ubaid <usmanubaid98@outlook.com>
2022-10-30 11:29:08 +0100

    Added DELETING Task from server functionality

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ function App() {
 
   // Delete task
   const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method: 'DELETE'
+    });
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
   
