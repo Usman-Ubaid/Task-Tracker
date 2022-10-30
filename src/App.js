1980bef093c6015f6dd88bcd59983d15dc90a7aa
Usman Ubaid <usmanubaid98@outlook.com>
2022-10-30 11:37:58 +0100

    Included Adding task to server functionality.

--- src/App.js
+++ src/App.js
@@ -36,10 +36,19 @@ function App() {
   }
   
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = {id, ...task };
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json();
+
+    setTasks([...tasks, data])
   }
   
 
